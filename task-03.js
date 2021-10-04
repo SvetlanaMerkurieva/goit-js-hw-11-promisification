1cd9263f0485a16cccee5be57f0100733222b57d
SvetlanaMerkurieva <merkureva83@gmail.com>
2021-10-04 22:37:56 +0300

    Решение задания 3

--- task-03.js
+++ task-03.js
@@ -36,29 +36,27 @@ const randomIntegerFromInterval = (min, max) => {
  const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
-  
-  const makeTransaction = (transaction, onSuccess, onError) => {
+
+const makeTransaction = (transaction) => {
     const delay = randomIntegerFromInterval(200, 500);
-    
     return new Promise ((resolve, reject) => {
       setTimeout(() => {
         const canProcess = Math.random() > 0.3;
     
         if (canProcess) {
-          resolve(transaction.id, delay);
+          resolve(`Transaction ${transaction.id} processed in ${delay}ms`);
         } else {
-          reject(transaction.id);
+          reject(`Error processing transaction ${transaction.id}. Please try again later.`);
         }
       }, delay);
     });
+};
+  const logSuccess = (result) => {
+    console.log(result);
   };
   
-  const logSuccess = (id, time) => {
-    console.log(`Transaction ${id} processed in ${time}ms`);
-  };
-  
-  const logError = id => {
-    console.warn(`Error processing transaction ${id}. Please try again later.`);
+  const logError = error => {
+    console.warn(error);
   }; 
  
  
